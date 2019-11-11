250115aea04be2c8177602d570f0439933e02a5c
Koichi Yasuoka <yasuoka@kanji.zinbun.kyoto-u.ac.jp>
2019-11-11 18:05:41 +0900

    trial release

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+import glob
+import os
+if os.uname().sysname.startswith("CYGWIN") and os.uname().machine!="x86_64":
+  pass
+else:
+  raise OSError("cabocha-cygwin32 only for 32-bit Cygwin")
+
+setuptools.setup(
+  name="cabocha-cygwin32",
+  version="0.1",
+  packages=setuptools.find_packages(),
+  data_files=[
+    ("local/bin",glob.glob("bin/*")),
+    ("local/libexec/cabocha",glob.glob("libexec/cabocha/*")),
+    ("local/lib",glob.glob("lib/*.*")),
+    ("local/etc",glob.glob("etc/*")),
+    ("local/include",glob.glob("include/*")),
+    ("local/share/man/man1",glob.glob("share/man/man1/*")),
+    ("local/lib/cabocha/model",glob.glob("lib/cabocha/model/*"))
+  ],
+  install_requires=["mecab-cygwin32@git+https://github.com/KoichiYasuoka/mecab-cygwin32"]
+)
